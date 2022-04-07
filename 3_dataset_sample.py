2330cc51a271f15fecb1cfecce68f17c0b5caa92
Weishuchong <524139952@qq.com>
2022-04-07 12:12:59 +0000

    hard_verb

--- 3_dataset_sample.py
+++ 3_dataset_sample.py
@@ -52,6 +52,8 @@ class dataset_util:
         #         self.dict_event_to_synset[event_node].append((self.G[synset_node][event_node]['arg'], synset_node)) #
         self.dict_synset_to_event = {}
         self.dict_event_to_synset = {}
+        self.vocab_id = {}
+        self.word_set = set()
         for synset_node in self.synset_nodes:
             if synset_node not in self.dict_synset_to_event:
                 self.dict_synset_to_event[synset_node] = []
@@ -61,31 +63,39 @@ class dataset_util:
                 arg = self.G[synset_node][event_node]["arg"].lower()
                 event_node = event_node.lower()
                 
+
+                for word in event_node.split(" "):
+                    self.word_set.add(word)
+
                 self.dict_synset_to_event[synset_node].append((arg, event_node))
                 if event_node not in self.dict_event_to_synset:
                     self.dict_event_to_synset[event_node] = []
-                else:
-                    self.dict_event_to_synset[event_node].append((arg, synset_node))
-                        
-            
+                self.dict_event_to_synset[event_node].append((arg, synset_node))
+
+        for i in self.word_set:
+            self.vocab_id[i] = len(self.vocab_id) + 1#9333个词 word_to_id   
                 
-                
-        file = open('results_save/dict_event_to_synset.pickle', 'wb')
+        file = open('results_save/Pickle_for_Event_CL/dict_event_to_synset.pickle', 'wb')
         pickle.dump(self.dict_event_to_synset, file)
         file.close()
         
-        file2 = open('results_save/dict_synset_to_event.pickle', 'wb')
+        file2 = open('results_save/Pickle_for_Event_CL/dict_synset_to_event.pickle', 'wb')
         pickle.dump(self.dict_synset_to_event, file2)
         file2.close()
-        print("dict_event_to_synset and dict_synset_to_event are saved well.")
+
+        file3 = open('results_save/Pickle_for_Event_CL/vocab_id.pickle', 'wb')
+        pickle.dump(self.vocab_id, file3)
+        file3.close()
+
+        logging.info("dict_event_to_synset, dict_synset_to_event, vocab_id are saved well.")
 
         return
         
         
     def dataset_sample(self, sample_num = 100):
         # n = 0
-        if os.path.exists("./results_save/verb_centric_triple_events.txt"):
-            os.remove("./results_save/verb_centric_triple_events.txt") 
+        if os.path.exists("./results_save/Pickle_for_Event_CL/verb_centric_triple_events.txt"):
+            os.remove("./results_save/Pickle_for_Event_CL/verb_centric_triple_events.txt") 
         
         
         for synset_node in self.synset_nodes:
@@ -151,7 +161,8 @@ class dataset_util:
                     neg_events_arg = []
                     for adj_neg_event in nx.all_neighbors(self.G, neg_events[index]):
                         neg_events_arg.append(self.G[adj_neg_event][neg_events[index]]['arg'])
-                
+                    if neg_events_arg == []:#不让落单的neg_event作为负例
+                        continue
                     lines = synset_node + " || " + event_positive_pair[0] + "<>" + self.G[synset_node][event_positive_pair[0]]['arg'] + " || " + event_positive_pair[1] + "<>" + self.G[synset_node][event_positive_pair[1]]['arg'] + " || " + neg_events[index] + "<>" + ",".join(neg_events_arg) + "\n"
                     write_lines.append(lines.lower())
                     # print(synset_node + " || " + event_positive_pair[0] + " || " + event_positive_pair[1] + " || " + neg_events[index] + "\n")
@@ -161,70 +172,86 @@ class dataset_util:
                 # pickle.dump(write_lines, file)
                 # file.close()
                 
-                with open ("results_save/verb_centric_triple_events.txt", mode = "a+", encoding = "utf-8") as f:#需要指定mode和encoding，否则an integer is required (got type str)
+                with open ("results_save/Pickle_for_Event_CL/verb_centric_triple_events.txt", mode = "a+", encoding = "utf-8") as f:#需要指定mode和encoding，否则an integer is required (got type str)
 
                     try:
                         f.writelines(write_lines)
                     except:
                         for write_line in write_lines:
                             logging.info(write_line)
-            
-            # del event_positive_pair
-            # del event_positive_pairs
-            # del neg_events
-            # del adjs_syn_node
-            
-            # for event_triple in zip(event_positive_pairs, neg_events):
-            #     # neg_events_arg = []
-            #     # for adj_neg_event in nx.all_neighbors(self.G, event_triple[1]):
-            #     #     neg_events_arg.append(self.G[adj_neg_event][event_triple[1]]['arg'])
-            #     # f.writelines(synset_node + " || " + event_triple[0][0] + "<>" + self.G[synset_node][event_triple[0][0]]['arg'] + " || " + event_triple[0][1] + "<>" + self.G[synset_node][event_triple[0][1]]['arg'] + " || " + event_triple[1] + "<>" + ",".join(neg_events_arg) + " \n")
-            #     f.writelines(synset_node + " || " + event_triple[0][0] + " || " + event_triple[0][1] + " || " + event_triple[1] + " \n")
-                
-                
-                
-                # n = n + 1
-                # if n > 5000000:
-                #     f.close()
-                #     f = open ("results_save/verb_centric_triple_events_{}.txt".format(n//5000000), "w")
         
         logging.info("Dataset is constructed well.")
 
         return
     
     
-    # #为超图准备
-    # def collect_event_synset_dict(self):
-    #     self.dict_event_to_synset = {}
-    #     self.dict_synset_to_event = {}
+    def hard_dataset_sample(self,sample_num = 100):
+        if os.path.exists("./results_save/Pickle_for_Event_CL/hard_verb_centric_triple_events.txt"):
+            os.remove("./results_save/Pickle_for_Event_CL/hard_verb_centric_triple_events.txt") 
         
-    #     for synset_node in self.synset_nodes:
-    #         adjs_syn_node = self.G.adj[synset_node]
-    #         event_nodes_adj_for_syn_nodes =  filter(self.att_event, adjs_syn_node)
-    #         self.dict_synset_to_event[synset_node] = []
+        hard_verb = set()
+        for line in open('./hard.txt'): 
+            event_arg = line.strip('\n').split('|')
+            hard_verb.add(event_arg[1].strip(' '))
+            hard_verb.add(event_arg[3].strip(' '))
+            hard_verb.add(event_arg[7].strip(' '))
+            hard_verb.add(event_arg[10].strip(' '))
+        hard_synset = set()
+        for synset_node in self.synset_nodes:
+            adjs_syn_node = self.G[synset_node]
+            for event_node in adjs_syn_node:
+                arg = self.G[synset_node][event_node]["arg"].lower()
+                if arg not in hard_verb:
+                    continue
+                else:
+                    hard_synset.add(synset_node)
+
+        logging.info("The len of hard_verb is {}".format(len(hard_verb)))
+        logging.info("The len of hard_synset is {}".format(len(hard_synset)))
+
+        for synset_node in hard_synset:
+            adjs_syn_node = self.G[synset_node]
+            event_nodes_adj_for_syn_nodes = list(filter(self.att_event, adjs_syn_node)) 
+            if  sample_num < len(event_nodes_adj_for_syn_nodes) :
+                sample_events = list(random.sample(event_nodes_adj_for_syn_nodes, k = sample_num))
+                event_positive_pairs = list(itertools.combinations(sample_events, 2))
+                neg_events = list(random.sample(self.event_nodes, k = len(event_positive_pairs)))
+            else:
+                event_positive_pairs = list(itertools.combinations(event_nodes_adj_for_syn_nodes, 2))
+                neg_events = list(random.sample(self.event_nodes, k = len(event_positive_pairs)))
+
+            assert len(neg_events) == len(event_positive_pairs)
+            write_lines = []
+            for index, event_positive_pair in enumerate(event_positive_pairs):
+                neg_events_arg = []
+                for adj_neg_event in nx.all_neighbors(self.G, neg_events[index]):
+                    neg_events_arg.append(self.G[adj_neg_event][neg_events[index]]['arg'])
+                if neg_events_arg == []:#不让落单的neg_event作为负例
+                    continue
+                lines = synset_node + " || " + event_positive_pair[0] + "<>" + self.G[synset_node][event_positive_pair[0]]['arg'] + " || " + event_positive_pair[1] + "<>" + self.G[synset_node][event_positive_pair[1]]['arg'] + " || " + neg_events[index] + "<>" + ",".join(neg_events_arg) + "\n"
+                write_lines.append(lines.lower())
+                # print(synset_node + " || " + event_positive_pair[0] + " || " + event_positive_pair[1] + " || " + neg_events[index] + "\n")
+
             
-    #         for event_node in event_nodes_adj_for_syn_nodes:
-    #             event_node = event_node.lower()
-    #             self.dict_synset_to_event[synset_node].append(event_node)
-                
-    #             if event_node not in self.dict_event_to_synset:
-    #                 self.dict_event_to_synset[event_node] = [synset_node]
-    #             else:
-    #                 self.dict_event_to_synset[event_node].append(synset_node)
-        
-    #     file = open('results_save/dict_event_to_synset.pickle', 'wb')
-    #     pickle.dump(self.dict_event_to_synset, file)
-    #     file.close()
-        
-    #     file2 = open('results_save/dict_synset_to_event.pickle', 'wb')
-    #     pickle.dump(self.dict_synset_to_event, file2)
-    #     print("dict_event_to_synset and dict_synset_to_event are saved well.")
-    #     file2.close()
+            # file = open('results_save/write_lines.pickle', 'wb')
+            # pickle.dump(write_lines, file)
+            # file.close()
+            
+            with open ("results_save/Pickle_for_Event_CL/hard_verb_centric_triple_events.txt", mode = "a+", encoding = "utf-8") as f:#需要指定mode和encoding，否则an integer is required (got type str)
+
+                try:
+                    f.writelines(write_lines)
+                except:
+                    for write_line in write_lines:
+                        logging.info(write_line)
+    
+        logging.info("Dataset is constructed well.")
 
         
 if __name__ == '__main__':
     logging.basicConfig(filename='./log/3_dataset_sample.log', format='%(asctime)s | %(levelname)s | %(message)s', level=logging.DEBUG, filemode='w') #有filename是文件日志输出,filemode是’w’的话，文件会被覆盖之前生成的文件会被覆盖
 
     du = dataset_util(path = "results_save/")
-    du.dataset_sample()
+    # du.dataset_sample()
     # du.collect_event_synset_dict()
+    du.hard_dataset_sample()
